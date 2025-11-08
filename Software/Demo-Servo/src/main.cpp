19a3dc8f7840178f7e6ee5dc02e4d543ca3d8a25
Noah Breit <nhbreit2@illinois.edu>
2025-11-08 13:17:11 -0800

    servo is moving... not in correct direction

--- Software/Demo-Servo/src/main.cpp
+++ Software/Demo-Servo/src/main.cpp
@@ -1,26 +1,55 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_PIN (2)
+#define OUT_PIN (13)
+
+void init_serial() {
+  Serial.begin(9600);
+  delay(1000);
+  while(!Serial);
+}
+
+void toggle_led(int pin) {
+  digitalWrite(pin, LOW);
+  delay(1000);
+  digitalWrite(pin, HIGH);
+  delay(1000);
+}
+
+void drive_parallax_servo_clockwise(int pin) {
+  // assume pin is LOW at start of function
+  delay(20);
+  digitalWrite(pin, HIGH);
+  delayMicroseconds(1300);
+  digitalWrite(pin, LOW);
+}
+
+void drive_parallax_servo_counterwise(int pin) {
+  // assume pin is LOW at start of function
+  delay(20);
+  digitalWrite(pin, HIGH);
+  delayMicroseconds(1700);
+  digitalWrite(pin, LOW);
+}
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  pinMode(4, INPUT); // D2
-  Serial.begin(9600);
-  while(!Serial);
+
+  // Setup Serial
+  init_serial();
+
+  // Setup Pins
+  pinMode(LED_PIN, OUTPUT); // D2
+  pinMode(OUT_PIN, OUTPUT); // D13
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(18, LOW);
+  toggle_led(LED_PIN);
+  drive_parallax_servo_clockwise(OUT_PIN);
   delay(1000);
-  digitalWrite(18, HIGH);
+  drive_parallax_servo_counterwise(OUT_PIN);
   delay(1000);
-  Serial.println("Hello!");
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println("Hello!");
 }
