0f484c377ee8e320b953daf37165432fa9e5ff7d
Peter-Giannetos <pg19@illinois.edu>
2025-11-08 14:42:15 -0600

    New code

--- Software/Demo-Heater/src/main.cpp
+++ Software/Demo-Heater/src/main.cpp
@@ -1,28 +1,34 @@
 #include <Arduino.h>
 
 #define LED_PIN (2)
+#define OUT_PIN (19)
 
-// put function declarations here:
-int myFunction(int, int);
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
   pinMode(LED_PIN, OUTPUT); // D2
   Serial.begin(9600);
-  while(!Serial);
+  delay(1000);
+  while(!Serial)
+  {
+    delay(1);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_PIN, LOW);
+  digitalWrite(OUT_PIN, LOW);
   delay(1000);
   digitalWrite(LED_PIN, HIGH);
+  digitalWrite(OUT_PIN, HIGH);
+
   delay(1000);
   Serial.println("Hello!");
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+
+
+
 }
